83f04345043e89165dffdfbd3faa21246e7e70f4
walmir91 <walmirlmjr@gmail.com>
2021-11-06 17:06:59 -0300

    categoria DELETE implementada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,7 +21,8 @@ import { CategoriaCreateComponent } from './components/views/categoria/categoria
 import { FormsModule } from '@angular/forms';
 import {MatInputModule} from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import {MatSnackBarModule} from '@angular/material/snack-bar'
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import { CategoriaDeleteComponent } from './components/views/categoria/categoria-delete/categoria-delete.component'
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import {MatSnackBarModule} from '@angular/material/snack-bar'
     NavComponent,
     HomeComponent,
     CategoriaReadComponent,
-    CategoriaCreateComponent
+    CategoriaCreateComponent,
+    CategoriaDeleteComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/views/categoria/categoria.service.ts
+++ src/app/components/views/categoria/categoria.service.ts
@@ -19,10 +19,20 @@ findAll(): Observable<Categoria[]>{
   return this.http.get<Categoria[]>(url)
 }//Método para apresentar em tela todas as categorias já criadas.
 
+findById(id: String): Observable<Categoria> {
+  const url = `${this.baseUrl}/categorias/${id}`
+  return this.http.get<Categoria>(url)
+}//Método para apresentar em tela a categoria com base no id da mesma.
+
 create(categoria: Categoria): Observable<Categoria>{
   const url = `${this.baseUrl}/categorias`
   return this.http.post<Categoria>(url, categoria);
 } // Método para criação de nova Categoria.
+
+delete(id: String): Observable<void>{
+  const url = `${this.baseUrl}/categorias/${id}`
+  return this.http.delete<void>(url)
+}
 
 mensagem(str: String): void{
   this._snack.open(`${str}`, 'OK', {
